476cadb9832537b116c89956376eb3899aa722ca
The-Basanta <basanta.815424@ac.tu.edu.np>
2026-09-20 22:29:03 +0545

    react-stream-web

--- learn-day1/src/components/PlayerBar.jsx
+++ learn-day1/src/components/PlayerBar.jsx
@@ -106,7 +106,7 @@ export default function PlayerBar({
         onMouseEnter={onVolumeEnter}
         onMouseLeave={onVolumeLeave}
       >
-
+        <span className="volume-device">●</span>
         <span className="volume-percent">{volume}%</span>
         {volumeHovered && (
           <input
